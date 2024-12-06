8ca694aee1f5c81dfe244b8fc6c275c261ef862d
pramod <pramod.pramod36@gmail.com>
2024-12-07 00:52:50 +0530

    added params

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,25 +1,25 @@
 def call(Map config) {
-    pipeline{
-    agent{
-        label "${config.label}"
-    }
-    tools{
-        maven "${config.maven}"
-    }
-    stages{
-        stage('SCM'){
-            steps{
-                git(
-                    'url': "${config.url}",
-                    'branch': "${config.branch}"
-                )
+    pipeline {
+        agent {
+            label "${config.label}"
+        }
+        tools {
+            maven "${config.maven}" // Correctly defined Maven tool
+        }
+        stages {
+            stage('SCM') {
+                steps {
+                    git(
+                        url: "${config.url}",
+                        branch: "${config.branch}"
+                    )
+                }
+            }
+            stage('BUILD') {
+                steps {
+                    sh "mvn ${config.goal}" // Ensure config.goal is defined correctly
+                }
             }
         }
-        stage('BUILD'){
-            steps{
-                sh "mvn ${config.goal}"
-            }
-        }
     }
-  }
 }
